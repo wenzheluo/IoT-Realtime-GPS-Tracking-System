5b1d7c661c31fdef13cb10f71c8fb3f18e796704
Wenzhe Luo <100318920+manzitlo@users.noreply.github.com>
2022-10-17 21:06:46 -0400

    Create Firebase_default.h

---
+++ Realtime GPS Tracking System/Firebase_default.h
@@ -0,0 +1,9 @@
+#include "Firebase_Arduino_WiFiNINA.h"
+
+#define FIREBASE_HOST "ece508-gps-tracker-default-rtdb.firebaseio.com"  //Realtime Database HOST
+#define FIREBASE_AUTH "WQmJLrmrtiRVt4RqBdO6eIa1iGQu5Ly13oI9QMzy"  
+#define WIFI_SSID "AndyC-Guest"
+#define WIFI_PASSWORD "Nijiushijinbulai"
+
+//Define Firebase data object
+FirebaseData firebaseData;
